3e120d9079a9f002dc61ef07b5bf6efd7d10e22d
Mira1624 <mira.kan100@gmail.com>
2024-12-22 14:38:44 -0800

    Update HelloWorld.java

--- src/day01/HelloWorld.java
+++ src/day01/HelloWorld.java
@@ -3,5 +3,8 @@ package day01;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hello my friends");
+        System.out.println("Hello Students");
+        
     }
 }
